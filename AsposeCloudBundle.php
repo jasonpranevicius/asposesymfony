4e2cecd0f8a4f874375b4baee758b8981409f9c4
asposeforcloud <cloud@aspose.com>
2014-05-12 14:37:55 +0500

    Create AsposeCloudBundle.php

---
+++ AsposeCloudBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Aspose\Bundle\CloudBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AsposeCloudBundle extends Bundle
+{
+	public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+    }
+}

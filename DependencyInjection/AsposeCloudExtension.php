d7193ed6b70d817bfef943b8c544e1777bb626bd
Richard <rvanlaak@gmail.com>
2014-12-18 11:33:54 +0100

    replace Bundle services.xml with services.yml and format according to Symfony Bundle guidelines

--- DependencyInjection/AsposeCloudExtension.php
+++ DependencyInjection/AsposeCloudExtension.php
@@ -20,10 +20,10 @@
  
 namespace Aspose\Bundle\CloudBundle\DependencyInjection;
 
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\Loader;
 
 
 /**
@@ -38,8 +38,8 @@ class AsposeCloudExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-		$loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
 
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
